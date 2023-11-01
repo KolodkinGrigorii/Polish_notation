a92f2b11f82399a467f2b077d5fd3abbd50323cc
Колодкин Григорий Владимирович <s22380292@unn.ru>
2023-11-01 14:21:35 +0300

    .

---
+++ include/example.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Lexema {
+private:
+	int type;
+	int num;
+public:
+	Lexema(int a) {
+		type = 0;
+		num = a;
+	}
+	Lexema(char c)
+		if ((c == '(') or (c == ')')) {
+			type
+		}
+
+}; 
